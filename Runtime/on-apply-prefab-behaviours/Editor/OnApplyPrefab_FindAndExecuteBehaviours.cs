938958d756840019fe6e768f3a284f34abe41b65
larrykirschner <larrykirschner@gmail.com>
2018-05-06 16:54:12 -0700

    initial commit

---
+++ Runtime/on-apply-prefab-behaviours/Editor/OnApplyPrefab_FindAndExecuteBehaviours.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace BeatThat.OnApplyPrefabBehaviours
+{
+	/// <summary>
+	/// Enables you to execute custom behaviours on prefabs when they've been applied.
+	/// 
+	/// When present, this class will bind to events for PrefabUtility.prefabInstanceUpdated
+	/// and then find and execute all <code>OnApplyPrefabBehaviour</code>s on the prefab (the prefab rather than the instance).
+	/// </summary>
+	[InitializeOnLoad]
+	internal static class OnApplyPrefab_FindAndExecuteBehaviours
+	{
+		static OnApplyPrefab_FindAndExecuteBehaviours()
+		{
+			PrefabUtility.prefabInstanceUpdated += OnPrefabInstanceUpdate;
+		}
+
+		private static List<GameObject> instancesUpdating = new List<GameObject> ();
+
+		static void OnPrefabInstanceUpdate(GameObject prefabInstance)
+		{
+			if (instancesUpdating.Contains (prefabInstance)) {
+				return;
+			}
+
+			instancesUpdating.Add (prefabInstance);
+
+			var prefab = PrefabUtility.GetPrefabParent(prefabInstance) as GameObject;
+
+			FindAndExecuteBehaviours_DepthFirst (prefab.gameObject, prefabInstance, prefab);
+
+			instancesUpdating.Remove (prefabInstance);
+
+			AssetDatabase.SaveAssets ();
+		}
+
+		private static void FindAndExecuteBehaviours_DepthFirst(GameObject cur, GameObject prefabInstance, GameObject prefab)
+		{
+			foreach (Transform child in cur.transform) {
+				FindAndExecuteBehaviours_DepthFirst (child.gameObject, prefabInstance, prefab);
+			}
+
+			using(var bComps = ListPool<OnApplyPrefabBehaviour>.Get()) {
+				cur.GetComponents<OnApplyPrefabBehaviour> (bComps, true);
+
+				foreach(var b in bComps) {
+					b.OnApplyPrefab_BeforeAllSiblings(prefabInstance, prefab);
+				}
+
+				foreach(var b in bComps) {
+					b.OnApplyPrefab(prefabInstance, prefab);
+				}
+
+				foreach(var b in bComps) {
+					b.OnApplyPrefab_AfterAllSiblings(prefabInstance, prefab);
+				}
+			}
+		}
+	}
+}
